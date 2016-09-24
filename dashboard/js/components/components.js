8f7ed6a1e0ca3de1dbdeb1ba863d6b00cad7dce8
Dmytro Shchurov <dmitri.shchurov@schoolbee.com>
2016-09-24 15:05:03 +0200

    added widget icon directive

--- dashboard/js/components/components.js
+++ dashboard/js/components/components.js
@@ -27,6 +27,46 @@
         };
     }
 
+    function ExaWidgetIconDirective($document){
+        return {
+            transclude: false,
+            restrict: 'A',
+            link: function (scope, element) {
+
+                var startX = 0, startY = 0, x = 0, y = 0;
+
+                element.on('mousedown', function (event) {
+                    // Prevent default dragging of selected content
+                    event.preventDefault();
+                    startX = event.pageX - x;
+                    startY = event.pageY - y;
+                    $document.on('mousemove', mousemove);
+                    $document.on('mouseup', mouseup);
+                });
+
+                function mousemove(event) {
+                    y = event.pageY - startY;
+                    x = event.pageX - startX;
+                    element.css({
+                        top: y + 'px',
+                        left: x + 'px'
+                    });
+                }
+
+                function mouseup() {
+                    startX = startY = 0;
+                    x = y = 0;
+                    element.css({
+                        top: '0px',
+                        left: '0px'
+                    });
+                    $document.off('mousemove', mousemove);
+                    $document.off('mouseup', mouseup);
+                }
+            }
+        };
+    }
+
     angular.module('dashboard.components', [])
         .component('exaPageTitle',
         {
@@ -38,7 +78,9 @@
             transclude: false,
             templateUrl: 'templates/components/exa-navigation-bar.html',
             controller: ['$rootScope', '$state', 'underscore', ExaNavigationBarController]
-        });
+        })
+        // exaWidgetIcon is implemented as a directive to control restriction - extra tag is not willed
+        .directive('exaWidgetIcon',['$document', ExaWidgetIconDirective]);
 
 })
 (window.angular);
